9fecbc0eeb174e231ac6990249089583ce4ba566
Aphra <oreofeojomo@gmail.com>
2022-12-26 12:55:06 +0100

    feat: minor changes: format configure,dels

--- src/app/emp-info/emp-info.component.ts
+++ src/app/emp-info/emp-info.component.ts
@@ -1,32 +1,29 @@
 import { Component } from '@angular/core';
-import {RecordsService} from "../records.service"
+import { RecordsService } from '../records.service';
 @Component({
   selector: 'app-emp-info',
   templateUrl: './emp-info.component.html',
   styleUrls: ['./emp-info.component.css'],
-  providers: [RecordsService]
+  providers: [RecordsService],
 })
 export class EmpInfoComponent {
+  infoReceived1: string[] = [];
+  infoReceived2: string[] = [];
+  infoReceived3: string[] = [];
 
-infoReceived1: string[]=[];
-infoReceived2: string[]=[];
-infoReceived3: string[]=[];
+  getInfoFromServiceClass2() {
+    this.infoReceived2 = this.rservice.geInfo2();
+  }
+  getInfoFromServiceClass1() {
+    this.infoReceived1 = this.rservice.geInfo1();
+  }
+  getInfoFromServiceClass3() {
+    this.infoReceived3 = this.rservice.geInfo3();
+  }
 
-getInfoFromServiceClass2(){ 
-  this.infoReceived2 = this.rservice.geInfo2()
-}
-getInfoFromServiceClass1(){ 
-  this.infoReceived1 = this.rservice.geInfo1()
-}
-getInfoFromServiceClass3(){ 
-  this.infoReceived3 = this.rservice.geInfo3()
-}
+  constructor(private rservice: RecordsService) {}
 
-constructor(private rservice: RecordsService) { 
-
-}
-
-updateInfo(frm: any){ 
-  this.rservice.addInfo(frm.value.location)
-}
+  updateInfo(frm: any) {
+    this.rservice.addInfo(frm.value.location);
+  }
 }
